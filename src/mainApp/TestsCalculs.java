8032e83175df79a1147fc1f668fa831230ec00d9
David Lavigueur <davidlavigueur@live.ca>
2019-03-13 12:37:58 -0400

    Finition de la classe TestCalsuls

--- src/mainApp/TestsCalculs.java
+++ src/mainApp/TestsCalculs.java
@@ -18,16 +18,19 @@ public class TestsCalculs {
 	static Client client3;
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		cantineTest= new Cantine();
 		client1 = new Client("Jean");
+		cantineTest.listeClient.add(client1);
 		client2 = new Client("Martin");
+		cantineTest.listeClient.add(client2);
 		client3 = new Client("Joe");
-		
+		cantineTest.listeClient.add(client3);
 	}
 
 
 	@Test 
 	public void testCalculFacture() {
-		double[] tabFacture = Cantine.calculTaxes(0.10, 0.05, 50.00);
+		double[] tabFacture = cantineTest.calculTaxes(0.10, 0.05, 50.00);
 		
 		assertEquals(5.00,tabFacture[0],0);
 
@@ -38,7 +41,7 @@ public class TestsCalculs {
 	
 	@Test 
 	public void testCalculFactureMontantNull() {
-		double[] tabFacture = Cantine.calculTaxes(0.10, 0.05, 0.00);
+		double[] tabFacture = cantineTest.calculTaxes(0.10, 0.05, 0.00);
 		
 		assertEquals(0.00,tabFacture[0],0);
 		
@@ -49,7 +52,7 @@ public class TestsCalculs {
 
 	@Test 
 	public void testCalculFactureMontant2() {
-		double[] tabFacture = Cantine.calculTaxes(0.10, 0.05, 27.35);
+		double[] tabFacture = cantineTest.calculTaxes(0.10, 0.05, 27.35);
 		
 
 		assertEquals(2.735,tabFacture[0],0);
@@ -61,7 +64,7 @@ public class TestsCalculs {
 	
 	@Test 
 	public void testCalculFactureMontant3() {
-		double[] tabFacture = Cantine.calculTaxes(0.10, 0.05, 6666.00);
+		double[] tabFacture = cantineTest.calculTaxes(0.10, 0.05, 6666.00);
 		
 		assertEquals(666.6,tabFacture[0],0);
 
@@ -72,7 +75,7 @@ public class TestsCalculs {
 	
 	@Test 
 	public void testCalculFactureMontant4() {
-		double[] tabFacture = Cantine.calculTaxes(0.10, 0.05, 0.0150);
+		double[] tabFacture = cantineTest.calculTaxes(0.10, 0.05, 0.0150);
 		
 		assertEquals(0.0015,tabFacture[0],0);
 
@@ -83,6 +86,6 @@ public class TestsCalculs {
 	
 	@Test
 	public void testTotal() {	
-		Cantine.calculTotal();
+		cantineTest.calculTotal();
 	}
 }
