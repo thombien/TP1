e596195bc43c01d4a651c3f855766a990475e788
David Lavigueur <davidlavigueur@live.ca>
2019-03-13 11:49:45 -0400

    Ajout de nouveaux tests

--- src/mainApp/TestsCalculs.java
+++ src/mainApp/TestsCalculs.java
@@ -18,44 +18,49 @@ public class TestsCalculs {
 
 
 	@Test 
-	public void testCalculFactureTPS() {
+	public void testCalculFacture() {
 		double[] tabFacture = Cantine.calculTaxes(0.10, 0.05, 50.00);
+		
 		assertEquals(5.00,tabFacture[0],0);
-	}
-	
-	@Test
-	public void testCalculFactureTVQ() {	
-		double[] tabFacture = Cantine.calculTaxes(0.10, 0.05, 50.00);
+
 		assertEquals(2.50,tabFacture[1],0);
-	}
-	
-	@Test
-	public void testCalculFactureMontant(){
-		double[] tabFacture = Cantine.calculTaxes(0.10, 0.05, 50.00);
+
 		assertEquals(50.00,tabFacture[2],0);
 	}
 	
 	@Test 
 	public void testCalculFactureMontantNull() {
 		double[] tabFacture = Cantine.calculTaxes(0.10, 0.05, 0.00);
+		
+		assertEquals(0.00,tabFacture[0],0);
+		
+		assertEquals(0.00,tabFacture[1],0);
+		
 		assertEquals(0.00,tabFacture[2],0);
 	}
 
 	@Test 
 	public void testCalculFactureMontant2() {
 		double[] tabFacture = Cantine.calculTaxes(0.10, 0.05, 27.35);
-		assertEquals(27.35,tabFacture[2],0);
-	}
-    @Test public void testCalculFactureTPS2() {
-		double[] tabFacture = Cantine.calculTaxes(0.10, 0.05, 27.35);
+		
+
 		assertEquals(2.735,tabFacture[0],0);
-    	
-    }
-    @Test public void testCalculFactureTVQ2() {
-		double[] tabFacture = Cantine.calculTaxes(0.10, 0.05, 27.35);
+
 		assertEquals(1.3675,tabFacture[1],0);
-    	
+		
+		assertEquals(27.35,tabFacture[2],0);
     }
+	@Test 
+	public void testCalculFactureMontant3() {
+		double[] tabFacture = Cantine.calculTaxes(0.10, 0.05, 6666.00);
+		
+		assertEquals(666.6,tabFacture[0],0);
+
+		assertEquals(333.3,tabFacture[1],0);
+		
+		assertEquals(6666.00,tabFacture[2],0);
+    }
+	
 	@Test
 	public void testTotal() {	
 		Cantine.calculTotal();
